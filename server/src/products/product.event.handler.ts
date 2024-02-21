93587a51ab8da4664c34e4899bcb02eba7c58956
agustom <agustin.macazzaga@codiced.ev>
2024-02-21 15:22:07 -0300

    transform function working fine

--- server/src/products/product.event.handler.ts
+++ server/src/products/product.event.handler.ts
@@ -1,10 +1,20 @@
 import { Injectable } from "@nestjs/common";
 import { OnEvent } from "@nestjs/event-emitter";
+import { Product } from "./schemas/product.schema";
 
 @Injectable()
 export class ProductEventHandler {
     @OnEvent('CSVDATA')
     handleCsvDataReceived(csvData: any[]) {
-        console.log("csvData", csvData)
+        const product = transform(csvData)
     }
+}
+
+const transform = (rawProduct: any): Product => {
+    const product = new Product()
+    product.name = rawProduct.fullDocument.nombre
+    product.brand = rawProduct.fullDocument.marca_nombre
+    product.countInStock =rawProduct.fullDocument.stock
+
+    return product
 }
