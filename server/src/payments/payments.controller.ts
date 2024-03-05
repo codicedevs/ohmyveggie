80263309463bc0110756a22b73946625c51c0efa
agustom <agustin.macazzaga@codiced.ev>
2024-03-05 10:07:23 -0300

    rid of sdk , payway example working fine

--- server/src/payments/payments.controller.ts
+++ server/src/payments/payments.controller.ts
@@ -1,40 +1,22 @@
 // payment.controller.ts
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { PaymentService } from './payments.service';
-import { FormDataDto, FormDataHandlePaymentDto } from './dto/form.data.dto';
-import sdk from './lib/sdk';
 
 
-@Controller('payments')
+
+@Controller('payment')
 export class PaymentController {
   constructor(private readonly paymentService: PaymentService) { }
 
-  @Get("/get-example-token")
-  async getToken() {
-    return await this.paymentService.exampleGetToken();
+
+  @Post("/token-request")
+  async requestToken(@Body() formData: any) { // pasar a DTO
+    return await this.paymentService.paymentTokenRequest(formData)
   }
-  @Post("/request-token")
-  async requestToken(@Body() formData: any) {
-    return await this.paymentService.requestToken(formData)
+
+  @Post('/confirm')
+  async confirm(@Body() paymentData: any): Promise<any> {
+    return await this.paymentService.confirm(paymentData);
   }
-  @Post('/request-payment')
-  async handlePaymentRequest(@Body() formData: any): Promise<any> {
-    return await this.paymentService.paymentRequest(formData);
-  }
-  @Post('/confirm-payment')
-  async confirmPayment(@Body() paymentData: any): Promise<any> {
-    return await this.paymentService.confirmPayment(paymentData);
-  }
-  /*
-  @Get(':user_id')
-  async getCardToken(@Param('user_id') userId: string): Promise<string> {
-    const token = await this.paymentService.exampleGetCardToken(userId, sdk);
-    return token;
-  }
-  @Post('request-tokenized')
-  async requestTokenized(@Body() formData: any): Promise<any> {
-    const response = await this.paymentService.requestTokenized(formData);
-    return response;
-  }
-  */
+
 }
--- server/src/payments/payments.service.ts
+++ server/src/payments/payments.service.ts
@@ -1,78 +1,42 @@
 import { Injectable } from '@nestjs/common';
 import axios from "axios"
-import { FormDataDto, FormDataHandlePaymentDto } from './dto/form.data.dto';
-var TokenDataModulo = require('./lib/token_data.js');
-var PaymentDataModulo = require("./lib/payment_data.js")
 var sdkModulo = require("./lib/sdk")
 
 @Injectable()
 export class PaymentService {
-    async exampleGetToken(): Promise<any> {
-        const args = {
-            card_number: '4507990000004905',
-            card_expiration_month: '08',
-            card_expiration_year: "28",
-            security_code: "775",
-            card_holder_name: "John Doe",
-            type: "dni",
-            number: "25123456",
-            apiKey: process.env.PRISMA_PUBLIC_API_KEY,
-            'Content-Type': "application/json",
-            'Cache-Control': "no-cache"
-        };
-        const tokenData = new TokenDataModulo.tokenData(args);
-        const argsToSend = tokenData.getJSON();
-        const response = await axios.post(process.env.PRISMA_ENDPOINT_DEVELOPER + "/tokens", argsToSend.data, { headers: argsToSend.headers })
-        return response.data
-    }
 
-    async requestToken(formData: any) {
-        const args = {
-            card_number: formData.card_number,
-            card_expiration_month: formData.card_expiration_month,
-            card_expiration_year: formData.card_expiration_year,
-            security_code: formData.security_code,
-            card_holder_name: formData.card_holder_name,
-            type: formData.type,
-            number: formData.number,
-            apiKey: formData.apiKey,
-            'Content-Type': "application/json",
-            'Cache-Control': "no-cache"
+    async paymentTokenRequest(form: any): Promise<any> {
+        const url = 'https://developers-ventasonline.payway.com.ar/api/v2/tokens';
+        const headers = {
+            'Content-Type': 'application/json',
+            'apikey': '96e7f0d36a0648fb9a8dcb50ac06d260',
+            'X-Consumer-Username': '28464383_public',
         };
-        const tokenData = new TokenDataModulo.tokenData(args);
-        const argsToSend = tokenData.getJSON();
+        const dataForm = {
+            card_number: form.card_number,
+            card_expiration_month: form.card_expiration_month,
+            card_expiration_year: form.card_expiration_year,
+            security_code: form.security_code,
+            card_holder_name: form.card_holder_name,
+            card_holder_birthday: form.card_holder_birthday,
+            card_holder_door_number: form.card_holder_door_number,
+            card_holder_identification: {
+                type: form.card_holder_identification_type,
+                number: form.card_holder_identification_number
+            },
+            fraud_detection: {
+                device_unique_identifier: form.fraud_detection.device_unique_identifier
+            }
+        }
         try {
-            const response = await axios.post(process.env.PRISMA_ENDPOINT_DEVELOPER + "/tokens", argsToSend.data, { headers: argsToSend.headers })
-            return response.data
+            const response = await axios.post(url, dataForm, { headers });
+            return response.data;
         } catch (error) {
-            console.error(error)
+            throw error.response.data;
         }
     }
 
-    async paymentRequest(formData: any): Promise<any> {
-        const args = {
-            site_transaction_id: formData.site_transaction_id,
-            token: formData.token,
-            user_id: formData.user_id,
-            payment_method_id: formData.payment_method_id,
-            bin: formData.bin,
-            amount: formData.amount,
-            currency: formData.currency,
-            installments: formData.installments,
-            description: formData.description,
-            payment_type: formData.payment_type,
-            sub_payments: [],
-            apiKey: formData.apiKey,
-            'Content-Type': 'application/json'
-        }
-        const sdk = await new sdkModulo.sdk(process.env.PRISMA_AMBIENTE, process.env.PRISMA_PUBLIC_API_KEY, formData.apiKey);
-        const paymentData = await new PaymentDataModulo.paymentData(args);
-        const paymentDataJSON = paymentData.getJSON()
-        const response = await sdk.payment(paymentDataJSON, (result: any, err: any) => { console.log("result", result, "err", err) });
-        return response
-    }
-
-    async confirmPayment(paymentData: any): Promise<any> {
+    async confirm(paymentData: any): Promise<any> {
         const stringDatos = paymentData.stringDatos;
         const data = JSON.parse(stringDatos);
         const paymentId = data.paymentId;
@@ -91,32 +55,5 @@ export class PaymentService {
         return paymentResult
 
     }
-    /*
-        async exampleGetCardToken(user_id: string, sdk: any): Promise<string> {
-            const args = {
-                data: {},
-                headers: {
-                    "apikey": process.env.PRISMA_PRIVATE_API_KEY,
-                    "Content-Type": "application/json",
-                    "Cache-Control": "no-cache"
-                }
-            };
-            const cardTokens = await sdk.cardTokens(args, user_id)
-            return cardTokens // devolver solo el token de la card en si
-        }
-    
-        async requestTokenized(formulario: any): Promise<any> {// hacer dto de este form
-            const args = {
-                token: formulario.cardToken,
-                security_code: formulario.security_code,
-                apiKey: formulario.apiKey,
-                'Content-Type': "application/json",
-                'Cache-Control': "no-cache"
-            };
-            const tokenData = new TokenDataModulo.tokenData(args);
-            const requestData = tokenData.getJSON();
-            const response = await axios.post(process.env.PRISMA_ENDPOINT_DEVELOPER + "/tokens", requestData);
-            console.log(response.data);
-            return response.data;
-        }*/
+
 }
