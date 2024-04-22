da7df6e7ad1e4079c92dbfb97478778b8952a607
amacazzagacodice <agustin.macazzaga@codice.dev>
2024-04-22 12:38:01 -0300

    try catch on preference controller

--- server/src/payments/payments-controller.ts
+++ server/src/payments/payments-controller.ts
@@ -1,41 +1,44 @@
-import { Controller, Post, Body } from '@nestjs/common';
-import { PaymentService } from './payments-service';
-import { OrderDocument } from 'src/orders/schemas/order.schema';
-import { NotificationData } from 'src/interfaces';
-import { OrdersService } from 'src/orders/services/orders.service';
+import { Controller, Post, Body } from "@nestjs/common";
+import { PaymentService } from "./payments-service";
+import { OrderDocument } from "src/orders/schemas/order.schema";
+import { NotificationData } from "src/interfaces";
+import { OrdersService } from "src/orders/services/orders.service";
 
-
-
-@Controller('payments')
+@Controller("payments")
 export class PaymentController {
-  constructor(private readonly paymentService: PaymentService) { }
+  constructor(private readonly paymentService: PaymentService) {}
 
-  @Post('preference')
+  @Post("preference")
   async createPreference(@Body() order: OrderDocument) {
-    const preference = await this.paymentService.createPreference(order);
-    return { preference };
+    try {
+      const preference = await this.paymentService.createPreference(order);
+      return { preference };
+    } catch (error) {
+      return { msg: "Error al crear preference" };
+    }
   }
 }
 @Controller("notifications")
 export class NotificationController {
-  constructor(private readonly paymentService: PaymentService, private readonly ordersService: OrdersService) { }
-
+  constructor(
+    private readonly paymentService: PaymentService,
+    private readonly ordersService: OrdersService
+  ) {}
   /**
-   * 
+   *
    * @param notification esta funcion toma la notificacion, que envia mercado pago como parametro, dentro de ella
-   * se encuentra el id de la compra que mercado pago tiene registrada, dentro de esa compra esta el id de nuestra orden de sistema, 
+   * se encuentra el id de la compra que mercado pago tiene registrada, dentro de esa compra esta el id de nuestra orden de sistema,
    * la cual una vez recuperada, actualiza el estado de la mencionada orden en nuestro sistema
-   * @returns 
+   * @returns
    */
   @Post("mercado-pago")
   async handleNotification(@Body() notification: NotificationData) {
-    const payment = await this.paymentService.getPayment(notification.data.id)
+    const payment = await this.paymentService.getPayment(notification.data.id);
     if (payment) {
-      const id = payment.external_reference
-      const orderUpdated = await this.ordersService.updatePaid(id)
-      await this.paymentService.sendEmailConfirmation(orderUpdated)
+      const id = payment.external_reference;
+      const orderUpdated = await this.ordersService.updatePaid(id);
+      await this.paymentService.sendEmailConfirmation(orderUpdated);
     }
-
-    return { payment } // ver que devuelve la funcion
+    return { payment }; // ver que devuelve la funcion
   }
 }
--- server/src/payments/payments-service.ts
+++ server/src/payments/payments-service.ts
@@ -1,17 +1,16 @@
-import { MercadoPagoConfig, Preference, Payment } from 'mercadopago';
-import { Injectable } from '@nestjs/common';
-import { OrderDocument } from 'src/orders/schemas/order.schema';
-import { EmailService } from 'src/email/email.service';
-import { OrdersService } from 'src/orders/services/orders.service';
-import { UsersService } from 'src/users/services/users.service';
-import { UserDocument } from 'src/users/schemas/user.schema';
-
+import { MercadoPagoConfig, Preference, Payment } from "mercadopago";
+import { Injectable } from "@nestjs/common";
+import { OrderDocument } from "src/orders/schemas/order.schema";
+import { EmailService } from "src/email/email.service";
+import { OrdersService } from "src/orders/services/orders.service";
+import { UsersService } from "src/users/services/users.service";
+import { UserDocument } from "src/users/schemas/user.schema";
 
 @Injectable()
 export class PaymentService {
   /**las dependencias externas no se inyectan directamente en el constructor */
   private readonly preference: Preference;
-  private readonly payment: Payment
+  private readonly payment: Payment;
   private readonly client: any;
 
   constructor(
@@ -20,44 +19,41 @@ export class PaymentService {
     private readonly usersService: UsersService
   ) {
     this.client = new MercadoPagoConfig({
-      accessToken: "TEST-6951506869962077-041109-9968d6f209a4180103b81b0d0e0d3223-1767060986", // token de usuario vendedor test
-      options: { timeout: 5000 }
+      accessToken:
+        "TEST-6951506869962077-041109-9968d6f209a4180103b81b0d0e0d3223-1767060986", // token de usuario vendedor test
+      options: { timeout: 5000 },
     });
     this.preference = new Preference(this.client);
     this.payment = new Payment(this.client);
   }
 
   async createPreference(order: OrderDocument) {
-    try {
-      const preferenceResult = this.preference.create({
-        body: {
-          external_reference: order._id,
-          items: order.orderItems.map((item) => {
-            return ({
-              id: item.productId,
-              title: item.name,
-              quantity: item.qty,
-              unit_price: item.price
-            })
-          })
-        }
-      })
-      console.log(preferenceResult)
-      return preferenceResult
-    } catch (error) {
-      console.error(error)
-    }
+    const preferenceResult = this.preference.create({
+      body: {
+        external_reference: order._id,
+        items: order.orderItems.map((item) => {
+          return {
+            id: item.productId,
+            title: item.name,
+            quantity: item.qty,
+            unit_price: item.price,
+          };
+        }),
+      },
+    });
+    console.log(preferenceResult);
+    return preferenceResult;
   }
 
   async getPayment(id: string) {
-    const paymentResult = this.payment.get({ id })
-    return paymentResult
+    const paymentResult = this.payment.get({ id });
+    return paymentResult;
   }
 
   async sendEmailConfirmation(order: OrderDocument) {
-    const userId = order.user.toString()
-    const userToSendEmail = await this.usersService.findById(userId)
-    // busque el usuario por id , recupere el correo electronico,  envie un correo 
+    const userId = order.user.toString();
+    const userToSendEmail = await this.usersService.findById(userId);
+    // busque el usuario por id , recupere el correo electronico,  envie un correo
     // indicando que la venta es exitosa, tanto a ese user como al administrador
   }
 }
