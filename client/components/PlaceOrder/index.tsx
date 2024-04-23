ee812a9453a60602cf9d78fd0b6029a415eaf962
Matías Trovant <mtrovant@gmail.com>
2024-04-23 15:00:50 -0300

    Cambio de estado Delivered

--- client/components/PlaceOrder/index.tsx
+++ client/components/PlaceOrder/index.tsx
@@ -48,18 +48,20 @@ const PlaceOrder = () => {
 
   return (
 
-    <section className="section-4">
+    <section className="section-4" style={{padding: "100px 400px 0px 400px"}}>
       <CheckoutSteps step1 step2/> 
-      <h1 className="heading-3">Orden</h1>
+      <h1 className="heading-3">Su orden</h1>
       <div className="columns-2 w-row">
-        <div className="column-5 w-col w-col-8">
+        <div className="column-5 w-col w-col-8 px-0">
           <div className="orderitem">
-            <h3 className="heading-2">Envío</h3>
+            <h3 className="heading-2" style={{marginLeft: 0}}>Datos de envío</h3>
             <div className="container-item-order">
 
             <div className="txtordersubitem">Nombre : {data?.name}</div>
             <div className="txtordersubitem">Email : {data?.email}</div>
             <div className="txtordersubitem">Dirección : {cart.data.shippingDetails.address}</div>
+            <div className="txtordersubitem">Horario de entrega : </div>
+            <div className="txtordersubitem">En caso de no existir stock: </div>
             </div>
             {/* <div className="deliveredpaid">Pedido no enviado</div>
             <div className="deliveredpaid true">Pedido enviado</div>  */}
@@ -99,24 +101,27 @@ const PlaceOrder = () => {
               </div>
           </div>
         </div>
-        <div className="column-6 w-col w-col-4">
-          <div className="ordersummary">
-            <div className="itemordersummary">
+        <div className="column-6 w-col w-col-4 ">
+          <div className="ordersummary px-4 d-flex" style={{width: 350}}>
+            <div className="itemordersummary d-flex col pb-2">
               <div className="txtitemordersummary">Cantidad de productos :</div>
               <div className="txtitemordersummary">{totalProductos}</div>
             </div>
-            <div className="itemordersummary">
-              <div className="txtitemordersummary">Total :</div>
-              <div className="txtitemordersummary">${(cart.data.itemsPrice)}</div>
+            <div className="itemordersummary pb-3" >
+              <div className="txtitemordersummary fs-3">Total :</div>
+              <div className="txtitemordersummary fs-3">${(cart.data.itemsPrice)}</div>
             </div>
+            
+
             <Button
               type="button"
               className="btn-block"
               disabled={cart.data.cartItems.length === 0}
               onClick={onPlaceOrderHandler}
-            >
-              Realizar pedido
+              >
+              Confirmar pedido
             </Button>
+            
             {/* <div className="div-block-25">Pagar</div> */}
           </div>
         </div>
