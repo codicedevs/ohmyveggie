6cd0647668c29665ce844b120798e3bfa8850a48
agustom <agustin.macazzaga@codiced.ev>
2024-05-16 15:44:14 -0300

    local-auth guard message in spanish

--- server/src/guards/local-auth.guard.ts
+++ server/src/guards/local-auth.guard.ts
@@ -19,7 +19,7 @@ export class LocalAuthGuard extends AuthGuard('local') {
       !isString(password) ||
       !isString(email)
     ) {
-      throw new BadRequestException('Please enter a valid email and password.');
+      throw new BadRequestException('Por favor ingrese e mail y contraseña validos.');
     }
 
     const parentCanActivate = (await super.canActivate(context)) as boolean;
