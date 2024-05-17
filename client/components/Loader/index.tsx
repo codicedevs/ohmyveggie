759ed8bc326f8787a9e2a899dbc9eabb03e92019
agustom <agustin.macazzaga@codiced.ev>
2024-05-17 13:13:11 -0300

    Dotloader typed correctly

--- client/components/Loader/index.tsx
+++ client/components/Loader/index.tsx
@@ -1,33 +1,20 @@
-import { CSSProperties } from 'react';
-import { Spinner } from 'react-bootstrap';
+import React from 'react';
 import { DotLoader } from 'react-spinners';
 
 interface LoaderProps {
-  options?: CSSProperties;
+  size?: number;
+  color?: string;
 }
 
-const Loader: React.FC<any> = ( {size, color}  ) => {
+const Loader: React.FC<LoaderProps> = ({ size, color }) => {
   return (
-    // <Spinner
-    //   animation="border"
-    //   role="status"
-    //   style={{
-    //     width: '100px',
-    //     height: '100px',
-    //     margin: 'auto',
-    //     display: 'block',
-    //     ...options,
-    //   }}
-    // >
-    //   <span className="sr-only">Loading</span>
-    // </Spinner>
-
     <DotLoader
-  color={!color? "#a2cca6" : color}
-  size={!size? 80 : size }
-
-/>
+      color={color || "#a2cca6"}
+      size={size || 80}
+    />
   );
 };
 
 export default Loader;
+
+
