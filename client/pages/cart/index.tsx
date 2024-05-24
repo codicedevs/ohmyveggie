a5234bd35469e01e16cb72accf576e6077e37850
agustom <agustin.macazzaga@codiced.ev>
2024-05-24 16:16:58 -0300

    cartNew

--- client/pages/cart/index.tsx
+++ client/pages/cart/index.tsx
@@ -1,14 +1,14 @@
 import SEO from '../../components/SEO';
-import Cart from '../../components/Cart';
 import { NextPage } from 'next';
 import { homeConfig } from '../../utils';
+import CartNew from '../../components/CartNew';
 
 const CartPage: NextPage = () => {
   return (
     <>
       <SEO {...homeConfig} />
       <main className="wrapper py-5">
-        <Cart />
+        <CartNew />
       </main>
     </>
   );
