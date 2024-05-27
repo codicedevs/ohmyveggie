1f2feb2f9b8e506bf3ad5cd339830a1265d30c38
agustom <agustin.macazzaga@codiced.ev>
2024-05-27 10:58:32 -0300

    rid of cartNew

--- client/pages/cart/index.tsx
+++ client/pages/cart/index.tsx
@@ -1,14 +1,14 @@
 import SEO from '../../components/SEO';
 import { NextPage } from 'next';
 import { homeConfig } from '../../utils';
-import CartNew from '../../components/CartNew';
+
 
 const CartPage: NextPage = () => {
   return (
     <>
       <SEO {...homeConfig} />
       <main className="wrapper py-5">
-        <CartNew />
+  
       </main>
     </>
   );
