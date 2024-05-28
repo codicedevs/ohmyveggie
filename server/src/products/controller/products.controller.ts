62e20d5b4962c2ed65293b1f37b80e19aa89b41f
agustom <agustin.macazzaga@codiced.ev>
2024-05-28 12:38:54 -0300

    deleted top rated controller & service on product

--- server/src/products/controller/products.controller.ts
+++ server/src/products/controller/products.controller.ts
@@ -30,11 +30,6 @@ export class ProductsController {
     return this.productsService.findMany(pageId, filter);
   }
   
-  @Get("topRated")
-  getTopRatedProducts() {
-    return this.productsService.findTopRated();
-  }
-
   @Get(":id")
   getProduct(@Param("id") id: string) {
     return this.productsService.findById(id);
