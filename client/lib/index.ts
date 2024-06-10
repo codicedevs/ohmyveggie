85f07de510a846cf1a90f25b4d331a1472ccd84e
agustom <agustin.macazzaga@codiced.ev>
2024-06-10 12:38:49 -0300

    lib

--- client/lib/index.ts
+++ client/lib/index.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const proshopAPI = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_SERVER_API_URL || "https://codice.dev:3029", 
+  baseURL: process.env.NEXT_PUBLIC_SERVER_API_URL || "https://codice.dev3029", 
 });
 // 'https://codice.dev:3029'
 // 'https://codice.dev:3029'
