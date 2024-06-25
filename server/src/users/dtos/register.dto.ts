f4dbb283e4d2665704c2cc654be579aef2986ab1
agustom <agustin.macazzaga@codiced.ev>
2024-06-25 11:04:49 -0300

    registerdto conditions changed

--- server/src/users/dtos/register.dto.ts
+++ server/src/users/dtos/register.dto.ts
@@ -3,7 +3,7 @@ import { IsEmail, IsString, MinLength, MaxLength } from 'class-validator';
 export class RegisterDto {
   @IsString()
   @MinLength(4, { message: 'Username is too short.' })
-  @MaxLength(20, { message: 'Username is too long.' })
+  @MaxLength(50, { message: 'Username is too long.' })
   name: string;
 
   @IsEmail({}, { message: 'Email address is not valid.' })
@@ -11,6 +11,6 @@ export class RegisterDto {
 
   @IsString()
   @MinLength(5, { message: 'Password is too short.' })
-  @MaxLength(20, { message: 'Please is too long.' })
+  @MaxLength(20, { message: 'Password is too long.' })
   password: string;
 }
