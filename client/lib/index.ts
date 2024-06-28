38532aafa51cb0881b445f9a5cd9992dce3ba377
Matías Trovant <mtrovant@gmail.com>
2024-06-28 13:59:33 -0300

    cambio por ip url

--- client/lib/index.ts
+++ client/lib/index.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const proshopAPI = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_SERVER_API_URL || 'https://ohmyveggie.com:4000', 
+  baseURL: process.env.NEXT_PUBLIC_SERVER_API_URL || 'https://67.205.136.19:4000', 
 });
 // 
 // 'https://codice.dev:3029'
