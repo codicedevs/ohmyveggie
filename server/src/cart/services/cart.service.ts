807365708bd5f8562c1f2cc3e81bc35f75b1d9a1
Matías Trovant <mtrovant@gmail.com>
2024-06-28 16:44:22 -0300

    borr

--- server/src/cart/services/cart.service.ts
+++ server/src/cart/services/cart.service.ts
@@ -17,7 +17,7 @@ interface AddCartItem {
 export class CartService {
   cart = new Cart().cart;
   addCartItem({ qty, productId, product }: AddCartItem): CartItem {
-    console.log(this.cart.cartItems)
+    console.log("Cart Items", this.cart.cartItems)
     if (!productId && !product)
       throw new BadRequestException("No id or product provided.");
 
@@ -45,18 +45,16 @@ export class CartService {
         return cartItem;
       } else {
         this.cart.cartItems.push(cartItem);
-        console.log("array", this.cart.cartItems);
+      
 
         return cartItem;
       }
     } else {
-      console.log("el carrito ", this.cart.cartItems);
-      console.log("el producto", productId);
+      
       const cartItem = this.cart.cartItems.find(
         (x) => x.productId === productId
       );
       try {
-        console.log("dentro del try", qty, cartItem);
         cartItem.qty = qty;
       } catch (e) {
         console.log(e, "este seria el error en el back?");
