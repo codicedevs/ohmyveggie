1c3d0fedcb847f8b98cb5249ef2702c6dda4f179
Matías Trovant <mtrovant@gmail.com>
2024-07-29 16:42:47 -0300

    Cambio en Node Script para traer los productos

--- server/src/utils/node-script.js
+++ server/src/utils/node-script.js
@@ -1,55 +1,149 @@
-const fs = require('fs');
-const path = require('path');
-const { exec } = require('child_process');
+const fs = require("fs");
+const path = require("path");
+const { exec, execSync } = require("child_process");
+const { MongoClient } = require("mongodb");
+const mongoose = require("mongoose");
+const { Schema, model } = mongoose;
 
 const csvFilePath = "C:\\Star POS Market\\export\\ARTICULOS.csv";
-const tsvFilePath = path.join("C:\\Star POS Market\\export", 'ARTICULOS.tsv');
+const tsvFilePath = path.join("C:\\Star POS Market\\export", "ARTICULOS.tsv");
 
-const convertCsvToTsv = (csvFilePath, tsvFilePath, callback) => {
-    fs.readFile(csvFilePath, 'utf8', (err, data) => {
-        if (err) {
-            console.error('Error leyendo el archivo CSV:', err);
-            return;
-        }
+const dbURL =
+  "mongodb+srv://matiastrovant:mhjjqrxCRldG7UAb@omvtest.72ckbwy.mongodb.net/";
+const dbNAME = "ohmyveggie";
 
-        // Reemplazar punto y coma por tabulación
-        const tsvData = data.replace(/;/g, '\t');
+const productSchema = new Schema(
+  {
+    name: String,
+    category: String,
+    image: String,
+    price: Number,
+    countInStock: Number,
+    externalId: Number,
+  },
+  { timestamps: true }
+);
 
-        fs.writeFile(tsvFilePath, tsvData, 'utf8', (err) => {
-            if (err) {
-                console.error('Error escribiendo el archivo TSV:', err);
-                return;
-            }
+const Product = model("Product", productSchema);
+//Funciones para manejar las colecciones
 
-            console.log('El archivo CSV ha sido convertido a TSV correctamente.');
-            callback();
-        });
-    });
+const transform = (rawProduct) => {
+  const product = {
+    externalId: rawProduct.id,
+    name: rawProduct.nombre,
+    countInStock: rawProduct.stock,
+    price: rawProduct.precio,
+  };
+
+  return product;
 };
 
-const importToMongo = () => {
-    const escapedFilePath = tsvFilePath.replace(/\\/g, '\\\\');
-    exec(`mongoimport --uri "mongodb+srv://agustinmacazzaga:{password}@ohmyveggie.4xaykot.mongodb.net/ohmyveggie" --collection external-products --type tsv --headerline --file "${escapedFilePath}" --upsert --upsertFields id`, (error, stdout, stderr) => {
-        if (error) {
-            console.error(`Error al ejecutar mongoimport: ${error.message}`);
-            return;
-        }
-        if (stderr) {
-            console.error(`stderr: ${stderr}`);
-            return;
-        }
-        console.log(`stdout: ${stdout}`);
-        console.log("Importación completada.");
+function transformEventHandler(db, fullDocument) {
+  const product = transform(fullDocument);
+  delete product._id;
+  const productCollection = db.collection("products");
+
+  return productCollection.updateOne(
+    { externalId: product.externalId }, //Operadores de MongoDB
+    { $set: product },
+    { upsert: true }
+  );
+}
+
+//Funciones pprincipales del script
+
+const convertCsvToTsv = (csvFilePath, tsvFilePath, callback) => {
+  fs.readFile(csvFilePath, "utf8", (err, data) => {
+    if (err) {
+      console.error("Error leyendo el archivo CSV:", err);
+      return;
+    }
+
+    // Reemplazar punto y coma por tabulación
+    const tsvData = data.replace(/;/g, "\t");
+
+    fs.writeFile(tsvFilePath, tsvData, "utf8", (err) => {
+      if (err) {
+        console.error("Error escribiendo el archivo TSV:", err);
+        return;
+      }
+
+      console.log("El archivo CSV ha sido convertido a TSV correctamente.");
+      callback();
     });
+  });
+};
+
+const importToMongo = async () => {
+  const escapedFilePath = tsvFilePath.replace(/\\/g, "\\\\");
+  const client = new MongoClient(dbURL, {
+    monitorCommands: true,
+  });
+  await client.connect();
+  const db = client.db(dbNAME);
+  const extProducts = db.collection("external-products");
+  const deleteProd = await extProducts.deleteMany({});
+
+  exec(
+    `mongoimport --uri "mongodb+srv://matiastrovant:mhjjqrxCRldG7UAb@omvtest.72ckbwy.mongodb.net/${dbNAME}?retryWrites=true&w=majority&appName=OMVtest" \
+     --collection external-products \
+     --type tsv \
+     --headerline \
+     --file "${escapedFilePath}" \
+     --upsert \
+     --upsertFields id`,
+
+    async (error, stdout, stderr) => {
+      if (error) {
+        console.error(`Error al ejecutar mongoimport: ${error.message}`);
+        return;
+      }
+      if (stderr) {
+        console.error(`stderr: ${stderr}`);
+      }
+      console.log(`stdout: ${stdout}`);
+      console.log("Importación completada.");
+
+      const arr = [];
+      const extProducts = await db
+        .collection("external-products")
+        .find({})
+        .toArray();
+
+      extProducts.forEach((element) => {
+        arr.push(transformEventHandler(db, element));
+      });
+
+      await Promise.all(arr).then((result) => console.log(result));
+
+      const products = await db.collection("products").find({}).toArray();
+
+      const extProductIds = new Set(
+        extProducts
+          .filter((product) => product.activo === "S")
+          .map((prod) => prod.id)
+      );
+
+      const delThisProds = products
+        .filter((p) => !extProductIds.has(p.externalId))
+        .map((p) => p.externalId);
+
+      await db
+        .collection("products")
+        .deleteMany({ externalId: { $in: delThisProds } });
+
+      return;
+    }
+  );
 };
 
 const handleFileChange = (curr, prev) => {
-    if (curr.mtime !== prev.mtime) {
-        console.log("\nEl archivo", csvFilePath, "fue modificado!");
-        console.log("El tipo de cambio fue: change");
+  if (curr.mtime !== prev.mtime) {
+    console.log("\nEl archivo", csvFilePath, "fue modificado!");
+    console.log("El tipo de cambio fue: change");
 
-        convertCsvToTsv(csvFilePath, tsvFilePath, importToMongo);
-    }
+    convertCsvToTsv(csvFilePath, tsvFilePath, importToMongo);
+  }
 };
 
 fs.watchFile(csvFilePath, { interval: 10000 }, handleFileChange);
